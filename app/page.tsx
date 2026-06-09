fe318e6ab130230686a042121ec9cb806d016b03
arya <piadrol0@gmail.com>
2026-06-10 01:27:29 +0330

    tab image added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
-import { Suspense } from "react"
-import { DateRequestFlow } from "@/components/date-request/date-request-flow"
+// import { Suspense } from "react"
+// import { DateRequestFlow } from "@/components/date-request/date-request-flow"
+
+// export default function Page() {
+//   return (
+//     <Suspense fallback={null}>
+//       <DateRequestFlow />
+//     </Suspense>
+//   )
+// }
+import InvitationWrapper from "@/components/date-request/invitation-wrapper"
 
 export default function Page() {
-  return (
-    <Suspense fallback={null}>
-      <DateRequestFlow />
-    </Suspense>
-  )
+  return <InvitationWrapper />
 }
---
+++ components/date-request/invitation-wrapper.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { DateRequestFlow } from "./date-request-flow"
+import Image from "next/image"
+
+export default function InvitationWrapper() {
+    const [showIntro, setShowIntro] = useState(true)
+
+    useEffect(() => {
+        const t = setTimeout(() => {
+            setShowIntro(false)
+        }, 3000)
+
+        return () => clearTimeout(t)
+    }, [])
+
+    return (
+        <>
+            <DateRequestFlow />
+
+            {showIntro && (
+                <div className="fixed flex-col gap-5 inset-0 z-50 flex items-center justify-center bg-black text-white text-2xl font-bold pointer-events-none ">
+                    تب مرورگرو چک کردی؟ 👀
+                    <Image src="/screen.png" alt="tab info" width={250} height={250} />
+                </div>
+            )}
+        </>
+    )
+}
