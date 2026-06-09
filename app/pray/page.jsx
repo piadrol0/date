62b37376d7de7b028cd88adca1a563ef7f514826
arya <piadrol0@gmail.com>
2026-06-09 14:17:47 +0330

    god pray plz

---
+++ app/pray/page.jsx
@@ -0,0 +1,22 @@
+"use client"
+
+import Image from "next/image"
+
+export default function InvitationStep() {
+
+
+
+
+    return (
+      <>
+      <div className="flex flex-wrap gap-4 justify-center">
+        <Image src="/1.jpg" alt="Description" width={500} height={300} />
+        <Image src="/2.jpg" alt="Description" width={500} height={300} />
+        <Image src="/3.jpg" alt="Description" width={500} height={300} />
+        <Image src="/6.jpg" alt="Description" width={500} height={300} />
+        <Image src="/7.jpg" alt="Description" width={500} height={300} />
+        <Image src="/5.webp" alt="Description" width={500} height={300} />
+      </div>
+      </>
+    )
+}
